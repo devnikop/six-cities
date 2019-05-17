201b5b0bb0f6dbf70437258bb4de31fdfb8dbef9
devnikop <petrov.petrov.ak@gmail.com>
2019-05-17 08:07:14 +0300

    add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,42 @@
+export const offers = [
+  {
+    placeName: `Beautiful &amp; luxurious apartment at great location`,
+    placeType: `Apartment`,
+    isPremium: true,
+    src: `img/apartment-01.jpg`,
+    price: {
+      value: 120,
+      text: `night`,
+    }
+  },
+  {
+    placeName: `Wood and stone place`,
+    placeType: `Private room`,
+    isPremium: false,
+    src: `img/room.jpg`,
+    price: {
+      value: 80,
+      text: `night`,
+    }
+  },
+  {
+    placeName: `Canal View Prinsengracht`,
+    placeType: `Apartment`,
+    isPremium: false,
+    src: `img/apartment-02.jpg`,
+    price: {
+      value: 132,
+      text: `night`,
+    }
+  },
+  {
+    placeName: `Nice, cozy, warm big bed apartment`,
+    placeType: `Apartment`,
+    isPremium: true,
+    src: `img/apartment-03.jpg`,
+    price: {
+      value: 180,
+      text: `night`,
+    }
+  }
+];
