3dcc32facc45d71cc1c5cba5a5c900647145ddfd
devnikop <petrov.petrov.ak@gmail.com>
2019-05-23 11:00:05 +0300

    add leaflet & update mocks

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -5,6 +5,7 @@ export const offers = [
     isPremium: true,
     src: `img/apartment-01.jpg`,
     price: 120,
+    coords: [52.3909553943508, 4.85309666406198],
   },
   {
     placeName: `Wood and stone place`,
@@ -12,6 +13,7 @@ export const offers = [
     isPremium: false,
     src: `img/room.jpg`,
     price: 80,
+    coords: [52.369553943508, 4.85309666406198],
   },
   {
     placeName: `Canal View Prinsengracht`,
@@ -19,12 +21,14 @@ export const offers = [
     isPremium: false,
     src: `img/apartment-02.jpg`,
     price: 132,
+    coords: [52.3909553943508, 4.929309666406198],
   },
   {
     placeName: `Nice, cozy, warm big bed apartment`,
     placeType: `Apartment`,
     isPremium: true,
     src: `img/apartment-03.jpg`,
-    price: 180
+    price: 180,
+    coords: [52.3809553943508, 4.939309666406198],
   }
 ];
