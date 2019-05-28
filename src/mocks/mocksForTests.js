0a6641b12032cb9d2ae32bfeb3e1bc5b0517d899
devnikop <petrov.petrov.ak@gmail.com>
2019-05-28 16:21:40 +0300

    update leaflet mock

--- src/mocks/mocksForTests.js
+++ src/mocks/mocksForTests.js
@@ -1,4 +1,31 @@
-const leafletMock = jest.genMockFromModule(`leaflet`);
+const leafletMock = {
+  icon() {
+    return {
+      iconUrl: `img/pin.svg`,
+      iconSize: [30, 30]
+    };
+  },
+  map() {
+    return {
+      setView: jest.fn(),
+    };
+  },
+  tileLayer() {
+    return {
+      addTo: jest.fn(),
+    };
+  },
+  layerGroup() {
+    return {
+      addTo: jest.fn(),
+    };
+  },
+  marker() {
+    return {
+      addTo: jest.fn(),
+    };
+  },
+};
 
 const offersArrayMock = [
   {
