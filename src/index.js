b56568787519e93c66d75ec35c60ffb03638cf8e
devnikop <petrov.petrov.ak@gmail.com>
2019-06-07 11:25:41 +0300

    add Redux Dev Tools in index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import {reducer} from './reducer';
 import App from './components/app/app.jsx';
 
 const init = () => {
-  const store = createStore(reducer);
+  const store = createStore(
+      reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
   ReactDOM.render(
       <Provider store={store}>
